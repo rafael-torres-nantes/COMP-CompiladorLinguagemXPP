24f7fd6a54ca45af2aa1139c52d2cd8cb8f915ea
rafael-torres-nantes <rafatorresnantes@gmail.com>
2025-11-10 12:46:25 -0400

    feat: enhance token recognition in scanner; add support for new operators and reserved words

--- part02_analise_sintatica/token.h
+++ part02_analise_sintatica/token.h
@@ -5,54 +5,51 @@ enum TokenType
 {
     UNDEFINED,             // 0  - Undefined token
     ID,                    // 1  - Identifier (e.g., variable names)
-    DIGIT,                 // 2  - Integer literal
-    LETTER,                // 3  - Letter [a-zA-Z]
-    INTEGER_CONSTANT,      // 4  - digit+
-    CHAR_CONSTANT,         // 5  - 'ch' | '\n' | '\0', onde ch é qualquer caractere ASCII imprimível (como especificado pela função isprint(), exceto \ e aspas simples.
-    STRING_CONSTANT,       // 6  - "(ch)*", onde ch é qualquer caractere ASCII imprimível (como especificado pela função isprint(), exceto aspas duplas e \n.
+    INTEGER_LITERAL,       // 2  - Integer literal (digit+)
+    STRING_LITERAL,        // 3  - String literal delimited by " and "
 
-    RELATIONAL_OPERATOR,   // 7  - Relational operator
-    EQUAL,                 // 8  - == (equal to)
-    NOT_EQUAL,             // 9  - != (not equal to)
-    GREATER_THAN,          // 10 - > (greater than)
-    LESS_THAN,             // 11 - < (less than)
-    GREATER_OR_EQUAL_THAN, // 12 - >= (greater or equal than)
-    LESS_OR_EQUAL_THAN,    // 13 - <= (less or equal than)
+    OP,                    // 4  - Operator
+    LESS_THAN,             // 5  - < (less than)
+    GREATER_THAN,          // 6  - > (greater than)
+    LESS_OR_EQUAL_THAN,    // 7  - <= (less or equal than)
+    GREATER_OR_EQUAL_THAN, // 8  - >= (greater or equal than)
+    PLUS_OPERATOR,         // 9  - + (addition)
+    MINUS_OPERATOR,        // 10 - - (subtraction)
+    MULTIPLY_OPERATOR,     // 11 - * (multiplication)
+    DIVIDE_OPERATOR,       // 12 - / (division)
+    MODULO_OPERATOR,       // 13 - % (modulo)
+    ASSIGNMENT,            // 14 - = (assignment)
+    EQUAL,                 // 15 - == (equal to)
+    NOT_EQUAL,             // 16 - != (not equal to)
 
-    LOGICAL_OPERATOR,      // 14 - Logical operator
-    AND_OPERATOR,          // 15 - && (logical AND)
-    OR_OPERATOR,           // 16 - || (logical OR)
-    NOT_OPERATOR,          // 17 - ! (logical NOT)
+    SEP,                   // 17 - Separator
+    LEFT_BRACKET,          // 18 - ( (left parenthesis)
+    RIGHT_BRACKET,         // 19 - ) (right parenthesis)
+    LEFT_SQUARE_BRACKET,   // 20 - [ (left square bracket)
+    RIGHT_SQUARE_BRACKET,  // 21 - ] (right square bracket)
+    LEFT_CURLY_BRACE,      // 22 - { (left curly brace)
+    RIGHT_CURLY_BRACE,     // 23 - } (right curly brace)
+    SEMICOLON,             // 24 - ; (semicolon)
+    DOT,                   // 25 - . (dot)
+    COMMA,                 // 26 - , (comma)
 
-    ARITHMETIC_OPERATOR,   // 18 - Arithmetic operator
-    PLUS_OPERATOR,         // 19 - + (addition)
-    MINUS_OPERATOR,        // 20 - - (subtraction)
-    MULTIPLY_OPERATOR,     // 21 - * (multiplication)
-    DIVIDE_OPERATOR,       // 22 - / (division)
+    // Reserved words
+    CLASS,                 // 27 - class
+    EXTENDS,               // 28 - extends
+    INT,                   // 29 - int
+    STRING,                // 30 - string
+    BREAK,                 // 31 - break
+    PRINT,                 // 32 - print
+    READ,                  // 33 - read
+    RETURN,                // 34 - return
+    SUPER,                 // 35 - super
+    IF,                    // 36 - if
+    ELSE,                  // 37 - else
+    FOR,                   // 38 - for
+    NEW,                   // 39 - new
+    CONSTRUCTOR,           // 40 - constructor
 
-    SEPARATOR,             // 23 - Separator
-    LEFT_BRACKET,          // 24 - ( (left parenthesis)
-    RIGHT_BRACKET,         // 25 - ) (right parenthesis)
-    LEFT_CURLY_BRACE,      // 26 - { (left curly brace)
-    RIGHT_CURLY_BRACE,     // 27 - } (right curly brace)
-    LEFT_SQUARE_BRACKET,   // 28 - [ (left square bracket)
-    RIGHT_SQUARE_BRACKET,  // 29 - ] (right square bracket)
-    SEMICOLON,             // 30 - ; (semicolon)
-    COMMA,                 // 31 - , (comma)
-
-    ASSIGNMENT,            // 32 - = (assignment)
-    RESERVED_WORD,         // 33 - Reserved word (e.g., if, else)
-    END_OF_FILE,           // 34 - End of file
-
-    // Add reserved words here
-    INT,                   // 35 - int
-    CHAR,                  // 36 - char
-    VOID,                  // 37 - void
-    IF,                    // 38 - if
-    ELSE,                  // 39 - else
-    WHILE,                 // 40 - while
-    FOR,                   // 41 - for
-    RETURN                 // 42 - return
+    END_OF_FILE            // 41 - End of file
 };
 
 class Token 
@@ -93,38 +90,45 @@ class Token
             static string typeNames[] = {
                 "UNDEFINED",
                 "ID",
-                "DIGIT",
-                "LETTER",
-                "INTEGER_CONSTANT",
-                "CHAR_CONSTANT",
-                "STRING_CONSTANT",
-                "RELATIONAL_OPERATOR",
-                "EQUAL",
-                "NOT_EQUAL",
-                "GREATER_THAN",
+                "INTEGER_LITERAL",
+                "STRING_LITERAL",
+                "OP",
                 "LESS_THAN",
-                "GREATER_OR_EQUAL_THAN",
+                "GREATER_THAN",
                 "LESS_OR_EQUAL_THAN",
-                "LOGICAL_OPERATOR",
-                "AND_OPERATOR",
-                "OR_OPERATOR",
-                "NOT_OPERATOR",
-                "ARITHMETIC_OPERATOR",
+                "GREATER_OR_EQUAL_THAN",
                 "PLUS_OPERATOR",
                 "MINUS_OPERATOR",
                 "MULTIPLY_OPERATOR",
                 "DIVIDE_OPERATOR",
-                "SEPARATOR",
+                "MODULO_OPERATOR",
+                "ASSIGNMENT",
+                "EQUAL",
+                "NOT_EQUAL",
+                "SEP",
                 "LEFT_BRACKET",
                 "RIGHT_BRACKET",
-                "LEFT_CURLY_BRACE",
-                "RIGHT_CURLY_BRACE",
                 "LEFT_SQUARE_BRACKET",
                 "RIGHT_SQUARE_BRACKET",
+                "LEFT_CURLY_BRACE",
+                "RIGHT_CURLY_BRACE",
                 "SEMICOLON",
+                "DOT",
                 "COMMA",
-                "ASSIGNMENT",
-                "RESERVED_WORD",
+                "CLASS",
+                "EXTENDS",
+                "INT",
+                "STRING",
+                "BREAK",
+                "PRINT",
+                "READ",
+                "RETURN",
+                "SUPER",
+                "IF",
+                "ELSE",
+                "FOR",
+                "NEW",
+                "CONSTRUCTOR",
                 "END_OF_FILE"
             };
             return typeNames[type];
